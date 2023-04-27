db19d66d57e0eaf946d219eec424a11cb9f9bb51
AmineBichiou <115020783+AmineBichiou@users.noreply.github.com>
2023-04-27 23:25:07 +0100

    Delete Film - Copy directory

--- Film - Copy/src/main/java/com/amine/film/entities/FilmProtection.java
+++
@@ -1,8 +0,0 @@
-package com.amine.film.entities;
-import org.springframework.data.rest.core.config.Projection;
-
-@Projection(name = "nomFilm", types = { Film.class })
-
-public interface FilmProtection {
-    public String getNomFilm();
-}
